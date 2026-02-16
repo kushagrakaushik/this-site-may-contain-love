979891dd5978569fcfba8a113414ea2755efaf6b
Kushagra Kaushik <kushagra.k25239@nst.rishihood.edu.in>
2026-02-16 23:37:45 +0530

    feat: add base 3D envelope flip + flap animation

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function flip(){
+let isEnvelopeOpen = false;
+
+function toggleEnvelopeState(){
+
     const envelope = document.getElementsByClassName("envelope")[0];
+    const back = document.getElementsByClassName("back")[0];
+    // const letter = document.getElementsByClassName("letter")[0];
+
+    // Flipping the envelope
+
     envelope.classList.toggle("flip");
-    
-    setTimeout(() => {
-        envelope.classList.add("open-flap");
-    }, 1000);
+    if (!isEnvelopeOpen) {
+        setTimeout(() => {
+            back.classList.toggle("flip");
+            envelope.classList.toggle("open-flap");
+        }, 500);
+    }
+
+    else {
+        envelope.classList.toggle("open-flap");
+        setTimeout(() => {
+            back.classList.toggle("flip");
+        }, 500);
+    }
+
+    isEnvelopeOpen = !isEnvelopeOpen;
 }
